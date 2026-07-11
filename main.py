4739bd6f2ef0614870251543327bb94583e2f5b3
Ameen Rameez <rasheedahamed498@gmail.com>
2026-07-12 00:41:53 +0530

    Step 1: Basic menu system

---
+++ main.py
@@ -0,0 +1,17 @@
+while True:
+    print("\n --- student management system ---")
+    print("1, add student")
+    print("2, view students")
+    print("3, exit")
+
+    choice = input("enter your choice: ")
+
+    if choice =="1":
+        print("add student selected")
+    elif choice == "2":
+        print("view students selected")
+    elif choice == "2":
+        print("existing program...")
+        break
+    else:
+        print("invalid choice. Try again...")
