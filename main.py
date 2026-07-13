d1778e8910035c6baa1dec3dba8a74e06f8d98ef
Ameen Rameez <rasheedahamed498@gmail.com>
2026-07-13 22:24:38 +0530

    first version of student management system

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
 while True:
-    print("\n --- student management system ---")
+    print("\n--- student management system ---")
     print("1, add student")
     print("2, view students")
     print("3, exit")
 
     choice = input("enter your choice: ")
 
-    if choice =="1":
-        print("add student selected")
+    
+    if choice == "1":
+        name = input("Enter student name: ")
+
+        with open("students.txt", "a") as file:
+            file.write(name + "\n")
+
+        print("Student added successfully!")
+
     elif choice == "2":
-        print("view students selected")
-    elif choice == "2":
-        print("existing program...")
+        try:
+            with open("students.txt", "r") as file:
+                students = file.readlines()
+
+            if students == []:
+                print("No students found.")
+            else:
+                print("Student list:")
+                for student in students:
+                    print(student.strip())
+
+        except FileNotFoundError:
+            print("No students found.")
+
+    
+    elif choice == "3":
+        print("Exiting program...")
         break
+
+    # INVALID INPUT
     else:
-        print("invalid choice. Try again...")
+        print("Invalid choice. Try again...")
